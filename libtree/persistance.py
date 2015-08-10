6bbcdb20ef1cf40b8a94fa5b7af579f337114afd
Fabian Kochem <fkochem@gmail.com>
2015-08-10 22:34:06 +0200

    check for postgres server version

--- libtree/persistance.py
+++ libtree/persistance.py
@@ -10,6 +10,8 @@ import os
 import psycopg2
 import psycopg2.extras
 
+REQUIRED_POSTGRES_VERSION = (9, 4, 0)
+
 
 class PostgreSQLPersistance(object):
     protocol = 'postgres'
@@ -21,6 +23,9 @@ class PostgreSQLPersistance(object):
 
         self._connection = connection
         self._cursor = cursor
+
+        self.check_postgres_version()
+        self.rollback()
 
     def __iter__(self):
         for iter in self._cursor:
@@ -47,6 +52,16 @@ class PostgreSQLPersistance(object):
     def get_last_row_id(self):
         self._cursor.execute("SELECT LASTVAL();")
         return self._cursor.fetchone()['lastval']
+
+    def check_postgres_version(self):
+        self._cursor.execute("SHOW server_version;")
+        result = self._cursor.fetchone()['server_version']
+        server_version = tuple(map(int, result.split('.')))
+        if server_version < REQUIRED_POSTGRES_VERSION:
+            msg = 'Insufficient Postgres version: {}, required: {}'
+            msg = msg.format('.'.join(map(str, server_version)),
+                             '.'.join(map(str, REQUIRED_POSTGRES_VERSION)))
+            raise EnvironmentError(msg)
 
     def install(self):
         self.create_schema()
