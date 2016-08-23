e81a0a495bbe2ad2d25c811f00a05a93b5a34436
Fabian Kochem <kochem@cat06.de>
2016-08-23 14:33:45 +0200

    Bump version: 5.1.0 -> 5.2.0

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ else:
 
 setup(
     name='libtree',
-    version='5.1.0',
+    version='5.2.0',
     author='Fabian Kochem',
     author_email='fabian.kochem@concepts-and-training.de',
     description='Python Tree Library',
